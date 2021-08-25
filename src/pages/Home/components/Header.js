5e9957c6710fd07f1622ab329c23d095c44e1819
DiegoRoney <46231316+DiegoRoney@users.noreply.github.com>
2021-08-25 18:52:06 -0300

    Update Header.js

--- src/pages/Home/components/Header.js
+++ src/pages/Home/components/Header.js
@@ -1,25 +1,24 @@
 import React from 'react';
 
 function Header() {
-    return(
-    <header className='header'>
-        <div className='toolbar'>
-            <div>
-                <a href='/'>Logo</a>
+    return (
+        <header className='header'>
+            <div className='toolbar'>
+                <div>
+                    <a href='/'>Logo</a>
+                </div>
+
+                <div>
+                    <button>Novo Post</button>
+                    <span>img1</span>
+                    <span>img2</span>
+                    <span>img3</span>
+                </div>
+
+
+
             </div>
-
-            <div>
-                <button>Novo Post</button>
-                <span>img1</span>
-                <span>img2</span>
-                <span>img3</span>
-            </div>
-            //deletar isso
-
-            <button className = 'button'>Novo Post</button>
-
-        </div>
-    </header>
+        </header>
     )
 }
 
